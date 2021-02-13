b9aafa4e3fb57e346e39d3905167e26defc2d484
htuA <enzo.rissip@gmail.com>
2021-02-13 02:34:48 -0400

    Login button added

---
+++ .js/valid.js
@@ -0,0 +1,11 @@
+function validated()
+{
+    var user = document.getElementsByTagName("username");
+    var pass = document.getElementsByTagName("password");
+
+if(user == "su_admin@wiki.com" && pass == "adminroot")
+{
+    alert("Hello Fellow Admin!")
+}
+
+}
